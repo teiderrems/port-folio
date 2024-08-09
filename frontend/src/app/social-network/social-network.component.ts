cc248ed5efffa32971f633ff78cd5cdf9fa8bd46
teida <raoul.teida@facsciences-uy1.cm>
2024-08-09 16:08:37 +0200

    update

---
+++ frontend/src/app/social-network/social-network.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-network',
+  standalone: true,
+  imports: [],
+  templateUrl: './social-network.component.html',
+  styleUrl: './social-network.component.css'
+})
+export class SocialNetworkComponent {
+
+}
